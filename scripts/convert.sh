5368d7b41e95f097279903e13629d0b20b436cdf
Yushun Xiang@H100 <Yushun.Xiang@foxmail.com>
2025-07-07 17:20:48 +0000

    change some scripts

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,6 +1,6 @@
 cd ..
 python -m ds_version_convert.convert_dataset_v20_to_v21 \
     --repo-id=lerobot/test \
-    --root=data/merge-data/pp-red-apple-pot-3 \
+    --root=data/merge-data/pour-orange-green \
     --num-workers=8 \
     --video-backend=pyav
--- scripts/merge.sh
+++ scripts/merge.sh
@@ -1,14 +1,18 @@
-BASE_DIR=data/origin-data/pp-red-apple-pot-3
+BASE_DIR=data/origin-data/pour-orange-green
 MID_DIR=lerobot/test
 cd ..
 python -m dataset_merging.merge_lerobot_dataset \
-    --sources \
-    $BASE_DIR/05-29-17-04-40/$MID_DIR \
-    $BASE_DIR/05-29-17-28-57/$MID_DIR \
-    $BASE_DIR/06-07-18-39-21/$MID_DIR \
-    $BASE_DIR/06-07-20-30-30/$MID_DIR \
-    $BASE_DIR/06-28-21-53-29/$MID_DIR \
-    --output data/merge-data/pp-red-apple-pot-3 \
+    --sources $BASE_DIR/07-07-22-53-59/$MID_DIR \
+    $BASE_DIR/07-07-23-37-58/$MID_DIR \
+    --output data/merge-data/pour-orange-green \
     --state_max_dim 8 \
     --action_max_dim 8 \
     --fps 30
+
+# python -m dataset_merging.merge_lerobot_dataset \
+#     --sources \
+#     $BASE_DIR/06-29-19-36-20/$MID_DIR \
+#     --output data/merge-data/pp-left-green-apple-pot \
+#     --state_max_dim 8 \
+#     --action_max_dim 8 \
+#     --fps 30
